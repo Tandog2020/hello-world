7e67884f4131dab5006348820d25af02334071c0
Tandog2020 <80129283+Tandog2020@users.noreply.github.com>
2021-03-05 16:54:05 -0500

    Revert "Update HelloWorld"
    This reverts commit d211d8b5f87c169544c79082267553ba04ae6d3e.

--- GitHubtest.cpp
+++ GitHubtest.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
+
+
 int main() {
 	cout << "Hello World";
-	cout << "Program modified, but able to revert to orignal version using GitHub Desktop.";
 	return 0;
 }
