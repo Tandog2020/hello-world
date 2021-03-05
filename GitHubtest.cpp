d211d8b5f87c169544c79082267553ba04ae6d3e
Tandog2020 <80129283+Tandog2020@users.noreply.github.com>
2021-03-05 16:53:06 -0500

    Update HelloWorld

--- GitHubtest.cpp
+++ GitHubtest.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
-
-
 int main() {
 	cout << "Hello World";
+	cout << "Program modified, but able to revert to orignal version using GitHub Desktop.";
 	return 0;
 }
